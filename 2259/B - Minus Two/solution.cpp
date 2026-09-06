77555da8e9e850d9add7f51a48214eba1b6ed046
Deep Kevadiya <deepkevadiya18@gmail.com>
2026-09-06 12:47:24 +0530

    Add Minus Two [B] from Codeforces

---
+++ 2259/B - Minus Two/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    int odd = 0;
+    int even0 = 0;
+    int even2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        long long x;
+        cin >> x;
+        if (x % 2 == 1)
+            odd++;
+        else if (x % 4 == 0)
+            even0++;
+        else
+            even2++;
+    }
+    cout << max({odd, even0, even2}) << '
+';
+}
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
