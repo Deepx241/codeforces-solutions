a1094863592eabf371558bb63b128b4741d589ac
Deep Kevadiya <deepkevadiya18@gmail.com>
2026-09-10 14:45:34 +0530

    Add Abraham's Great Escape [B] from Codeforces

---
+++ 2155/B - Abraham's Great Escape/solution.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+ 
+        if (k == n * n - 1) {
+            cout << "NO
+";
+        } 
+        else {
+            cout << "YES
+";
+ 
+            for (int i = 0; i < n; i++) {
+                string s = "";
+                for (int j = 0; j < n; j++) {
+ 
+                    if (k > 0) {
+                        s += 'U';
+                        k--;
+                    }
+                    else if (i == n - 1 && j == n - 1) {
+                        s += 'L';
+                    }
+                    else if (i == n - 1) {
+                        s += 'R';
+                    }
+                    else {
+                        s += 'D';
+                    }
+                }
+ 
+                cout << s << '
+';
+            }
+        }
+    }
+ 
+    return 0;
+}
